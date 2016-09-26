8ad991e9a9ff34d430de88712b88a4df3521387f
Oleg Blokhin <blohin.o.d@gmail.com>
2016-09-26 15:23:43 +0300

    Refactoring

--- store.js
+++ store.js
@@ -1,15 +1,14 @@
-"use strict"
+"use strict";
 
 function statement(customer, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
   for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
     let thisAmount = 0;
 
     // determine amount for each movie
-    switch (movie.code) {
+    switch (movieFor(r).code) {
       case "regular":
         thisAmount = 2;
         if (r.days > 2) {
@@ -30,10 +29,10 @@ function statement(customer, movies) {
     //add frequent renter points
     frequentRenterPoints++;
     // add bonus for a two day new release rental
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
+    if (movieFor(r).code === "new" && r.days > 2) frequentRenterPoints++;
 
     //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n`;
+    result += `\t${movieFor(r).title}\t${thisAmount}\n`;
     totalAmount += thisAmount;
   }
   // add footer lines
@@ -41,6 +40,8 @@ function statement(customer, movies) {
   result += `You earned ${frequentRenterPoints} frequent renter points\n`;
 
   return result;
+
+  function movieFor(rental) {return movies[rental.movieID];}
 }
 
 let customer = {
@@ -52,7 +53,7 @@ let customer = {
     "movieID": "F002",
     "days": 1
   }, ]
-}
+};
 
 let movies = {
   "F001": {
@@ -64,6 +65,6 @@ let movies = {
     "code": "regular"
   },
   // etc
-}
+};
 
-console.log(statement(customer, movies))
+console.log(statement(customer, movies));
