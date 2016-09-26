4a623e7b060d4142997a4ef9b677f191776cde40
Oleg Blokhin <blohin.o.d@gmail.com>
2016-09-26 15:56:51 +0300

    Refactoring

--- store.js
+++ store.js
@@ -1,26 +1,34 @@
 "use strict";
 
 function statement(customer, movies) {
-    let totalAmount = 0;
-    let frequentRenterPoints = 0;
     let result = `Rental Record for ${customer.name}\n`;
 
     for (let r of customer.rentals) {
-        frequentRenterPoints += frequentRenterPointsFor(r);
-    }
-    for (let r of customer.rentals) {
         result += `\t${movieFor(r).title}\t${amountFor(r)}\n`;
     }
-    for (let r of customer.rentals) {
-        totalAmount += amountFor(r);
-    }
-    // add footer lines
-    result += `Amount owed is ${totalAmount}\n`;
-    result += `You earned ${frequentRenterPoints} frequent renter points\n`;
-
+    result += `Amount owed is ${totalAmount()}\n`;
+    result += `You earned ${totalFrequentRenterPoints()} frequent renter points\n`;
     return result;
 
-    function movieFor(rental) {return movies[rental.movieID];}
+    function totalFrequentRenterPoints() {
+        let result = 0;
+        for (let r of customer.rentals) {
+            result += frequentRenterPointsFor(r);
+        }
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let r of customer.rentals) {
+            result += amountFor(r);
+        }
+        return result;
+    }
+
+    function movieFor(rental) {
+        return movies[rental.movieID];
+    }
 
     function amountFor(r) {
         let thisAmount = 0;
@@ -60,7 +68,7 @@ let customer = {
     }, {
         "movieID": "F002",
         "days": 1
-    }, ]
+    },]
 };
 
 let movies = {
